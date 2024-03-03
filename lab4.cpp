30cfbf80c5396efb03b9107380087ac5cfffe933
Evgeniy Malyakshin <bigkeksik@bk.ru>
2024-03-03 21:20:11 +0500

    add base function

--- lab4.cpp
+++ lab4.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
+int** createMatrix(int length) {
+    int** matrix = new int* [length];
+    for (int i = 0; i < length; i++) {
+        matrix[i] = new int[length];
+        for (int j = 0; j < length; j++) {
+            matrix[i][j] = rand() % 10;
+        }
+    }
+        
+    return matrix;
+}
+
+void printMatrix(int** matrix, int length) {
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length; j++) {
+            cout << matrix[i][j] << ' ';
+        }
+        
+        cout << endl;
+    }
+}
+
 int main() {
-    cout << "Hello, world!" << endl;
+    int length = 3;
+    int** matrix = createMatrix(length);
+    
+    printMatrix(matrix, length);
+    
+    return 0;
 }
